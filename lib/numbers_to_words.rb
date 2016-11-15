c9f2c8c080389dfdcd2c262e2a2b3ad644e050c4
Eric Bomblatus and Michael Andrade <eric+michael@>
2016-11-15 10:25:00 -0800

    pass spec 2

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,5 +1,16 @@
-class Int
+class Fixnum
   define_method(:numbers_to_words) do
-    #do computing!
+    number_words = Hash.new()
+    number_words.store(1, "one")
+    number_words.store(2, "two")
+    number_words.store(3, "three")
+    number_words.store(4, "four")
+    number_words.store(5, "five")
+    number_words.store(6, "six")
+    number_words.store(7, "seven")
+    number_words.store(8, "eight")
+    number_words.store(9, "nine")
+
+    number_words.fetch(self)
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -1,8 +1,11 @@
 require('rspec')
 require('numbers_to_words')
 
-describe('Int#numbers_to_words') do
+describe('Fixnum#numbers_to_words') do
   it('returns a string for an integer') do
     expect(1.numbers_to_words).to(eq("one"))
+  end
+  it('returns a string for any single-digit integer') do
+    expect(5.numbers_to_words).to(eq("five"))
   end
 end
