913955afbe4c0a5f3bcacaeddd7544e3ae578cca
Eric Bomblatus and Michael Andrade <michael@>
2016-11-15 13:49:40 -0800

    going back through specs, pass new spec 2

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,7 +1,6 @@
 class Fixnum
   define_method(:numbers_to_words) do
     number_words = Hash.new()
-    number.words.store("0", "")
     number_words.store("1", "one")
     number_words.store("2", "two")
     number_words.store("3", "three")
@@ -22,31 +21,42 @@ class Fixnum
     number_words.store("18", "eighteen")
     number_words.store("19", "nineteen")
     number_words.store("20", "twenty")
-    number_words.store("30", "thirty")
-    number_words.store("40", "fourty")
-    number_words.store("50", "fifty")
-    number_words.store("60", "sixty")
-    number_words.store("70", "seventy")
-    number_words.store("80", "eighty")
-    number_words.store("90", "ninety")
+    # number_words.store("30", "thirty")
+    # number_words.store("40", "fourty")
+    # number_words.store("50", "fifty")
+    # number_words.store("60", "sixty")
+    # number_words.store("70", "seventy")
+    # number_words.store("80", "eighty")
+    # number_words.store("90", "ninety")
 
-    digits = self.divmod(10)
     output_string = ""
-    digits.each() do |digit|
-      counter = 0
-      digit_string = digit.to_s
-      puts digit_string
-      counter.times() do
-        digit_string = digit_string + "0"
-        puts digit_string
-      end
-      new_string = number_words.fetch(digit_string)
-      if (counter > 0 && new_string != "0")
-        new_string = new_string + "-"
-      end
-      output_string = new_string + output_string
-      counter += 1
-    end
-    output_string
+    output_strings = []
+    number_string = self.to_s
+    new_string = number_words.fetch(number_string)
+    new_string
+    # output_strings.unshift(new_string)
+    # output_string = output_strings.join("-")
+    # output_string
+
+    # number_words.fetch(self.to_s)
+    # digits = self.divmod(10)
+    # output_string = ""
+    # digits.each() do |digit|
+    #   counter = 0
+    #   digit_string = digit.to_s
+    #   puts digit_string
+    #   counter.times() do
+    #     digit_string = digit_string + "0"
+    #     puts digit_string
+    #   end
+    #   new_string = number_words.fetch(digit_string)
+    #   if (counter > 0 && new_string != "0")
+    #     new_string = new_string + "-"
+    #   end
+    #   output_string = new_string + output_string
+    #   counter += 1
+    # end
+    # output_string
+
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -2,16 +2,45 @@ require('rspec')
 require('numbers_to_words')
 
 describe('Fixnum#numbers_to_words') do
-  it('returns a word string for a digit string') do
+  it('returns the string one for 1') do
     expect(1.numbers_to_words).to(eq("one"))
   end
-  it('returns a word string for a digit string up to 9') do
-    expect(5.numbers_to_words).to(eq("five"))
-  end
-  it('returns a word string for a digit string up to 20') do
+  it('returns any single digit string up to 20') do
     expect(14.numbers_to_words).to(eq("fourteen"))
   end
-  it('returns a word string for a digit string up to 99') do
-    expect(33.numbers_to_words).to(eq("thirty-three"))
-  end
+  # it('returns a word string for a digit string up to 99') do
+  #   expect(33.numbers_to_words).to(eq("thirty-three"))
+  # end
 end
+
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# 19
+# 20
+# 2# (1)
+#
+# 1#(00)
+# 2 #(00)
+# 999
+# 1(000)
+# 10(000)
+# 100(000)
+# 1(000)(000)
+# 1(000)(000)(000)
