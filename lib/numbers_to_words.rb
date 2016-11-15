550f1de2b2accbf0761806229631c25c8aba6e02
Eric Bomblatus and Michael Andrade <eric@>
2016-11-15 10:32:38 -0800

    pass spec 3

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -10,6 +10,17 @@ class Fixnum
     number_words.store(7, "seven")
     number_words.store(8, "eight")
     number_words.store(9, "nine")
+    number_words.store(10, "ten")
+    number_words.store(11, "eleven")
+    number_words.store(12, "twelve")
+    number_words.store(13, "thirteen")
+    number_words.store(14, "fourteen")
+    number_words.store(15, "fifteen")
+    number_words.store(16, "sixteen")
+    number_words.store(17, "seventeen")
+    number_words.store(18, "eighteen")
+    number_words.store(19, "nineteen")
+    number_words.store(20, "twenty")
 
     number_words.fetch(self)
   end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -8,4 +8,7 @@ describe('Fixnum#numbers_to_words') do
   it('returns a string for any single-digit integer') do
     expect(5.numbers_to_words).to(eq("five"))
   end
+  it('returns a string for integer up to 20') do
+    expect(14.numbers_to_words).to(eq("fourteen"))
+  end
 end
